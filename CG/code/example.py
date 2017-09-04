4f267ad95a7ed3e320fb2324c4a2d175c5b52a5b
JonasMartins <jonas.s.martins@hotmail.com>
2017-09-04 19:47:15 -0300

    cube colors 12:25

--- CG/code/example.py
+++ CG/code/example.py
@@ -7,7 +7,7 @@ from time import time
 start = time() 
 elapsed = 0
 
-def rotate2d(pos, rad): x,y=pos; s,c = math.sin(rad), math.cos(rad); return (x*c)-(y*s), (y*c)+(x*s) 
+def rotate2d(pos, rad): x,y=pos; s,c = math.sin(rad), math.cos(rad); return (x*c)-(y*s),(y*c)+(x*s) 
 
 class Cam:
   def __init__(self,pos=(0,0,0),rot=(0,0)):
@@ -16,9 +16,9 @@ class Cam:
 
   def events(self, event):
     if event.type == pygame.MOUSEMOTION:
-      x,y = event.rel
-      #200 is speed
-      x/=5; y/=5
+      x,y = event.rel; x/=5000; y/=5000
+      print ('x ', x)
+      print ('y ',  y)
       self.rot[0]+=y; self.rot[1]+=x
 
 
@@ -29,18 +29,24 @@ class Cam:
     if key[pygame.K_q]: self.pos[1]+=s 
     if key[pygame.K_e]: self.pos[1]-=s
 
-    if key[pygame.K_UP]: self.pos[2]+=s 
-    if key[pygame.K_DOWN]: self.pos[2]-=s
-    if key[pygame.K_LEFT]: self.pos[0]-=s
-    if key[pygame.K_RIGHT]: self.pos[0]+=s
+    x,y = s*math.sin(self.rot[1]), s*math.cos(self.rot[1])
+
+    if key[pygame.K_UP]: self.pos[0]+=x; self.pos[2]+=y 
+    if key[pygame.K_DOWN]: self.pos[0]-=x; self.pos[2]-=y
+    if key[pygame.K_LEFT]: self.pos[0]-=y; self.pos[2]+=x
+    if key[pygame.K_RIGHT]: self.pos[0]+=y; self.pos[2]-=x
 
 pygame.init()
-w,h = 600, 600; cx,cy = w//2, h//2
+w,h = 800, 600; cx,cy = w//2, h//2
 screen = pygame.display.set_mode((w,h))
 clock = pygame.time.Clock() 
 
 verts = (-1,-1,-1),(1,-1,-1),(1,1,-1),(-1,1,-1),(-1,-1,1),(1,-1,1),(1,1,1),(-1,1,1)
-edges = (0,1),(1,2),(2,3),(3,0),(4,5),(5,6),(6,7),(7,4),(0,4),(1,5),(2,6),(3,7)
+# edges = (0,1),(1,2),(2,3),(3,0),(4,5),(5,6),(6,7),(7,4),(0,4),(1,5),(2,6),(3,7)
+
+faces = (1,2,3),(4,5,6,7),(0,1,5,4),(2,3,7,6),(0,3,7,4),(1,2,6,5)
+
+colors = (255,0,0),(255,128,0),(255,255,0),(255,255,255),(0,0,255),(0,255,0)
 
 cam = Cam((0,0,-5))
 
@@ -51,35 +57,51 @@ pygame.mouse.set_visible(1); pygame.event.set_grab(0)
 
 # o programa nunca passara de 10 segundos! 
 
-while elapsed < 10:
+while elapsed < 20:
 #while True:   
   dt = clock.tick(30)
 
   for event in pygame.event.get():
     if event.type == pygame.QUIT: pygame.quit(); sys.exit()
+    if event.type == pygame.KEYDOWN:
+      if event.key == pygame.K_ESCAPE: pygame.quit(); sys.exit()
     cam.events(event)
 
 
-  screen.fill((255,255,255))
+  screen.fill((0,0,0))
 
-  for edge in edges:
-    points = []
-    for x,y,z  in (verts[edge[0]],verts[edge[1]]):
-      
-      x-=cam.pos[0]
-      y-=cam.pos[1]
-      z-=cam.pos[2]
-      
-      # rotacionando no eixo y depois no eixo x 
-      x,z = rotate2d((x,z),cam.rot[1])
-      y,z = rotate2d((y,z),cam.rot[0])
-
-      f = 200/z
-      x,y = x*f, y*f
-      points+=[(cx+int(x), cy+int(y))]
-
-    pygame.draw.line(screen,(0,0,0), points[0],points[1],1)
   
+  vert_list = []; screen_coords = []
+  for x,y,z in verts:
+  
+    x-=cam.pos[0];y-=cam.pos[1];z-=cam.pos[2]    
+    x,z = rotate2d((x,z),cam.rot[1]);y,z = rotate2d((y,z),cam.rot[0])    
+    vert_list += [(x,y,z)]
+
+    f = 200/(z+0.01)
+    x,y = x*f, y*f
+    screen_coords+=[(cx+int(x), cy+int(y))]
+
+
+  face_list = []; face_color = []; depth = []
+
+  for f in range(len(faces)):
+    
+    face = faces[f]
+
+    on_screen = False
+    for i in face:
+      if vert_list[i][2]>0: on_screen = True; break 
+    if on_screen:
+      coords = [screen_coords[i] for i in face]
+      face_list += [coords]
+      face_color += [colors[f]]
+      depth += [sum(sum(vert_list[j][i] for j in face)**2 for i in range(3))]
+
+  order = sorted(range(len(face_list)),key=lambda i:depth[i],reverse=1)
+  
+  for i in order:
+    pygame.draw.polygon(screen,face_color[i],face_list[i])
 
   pygame.display.flip()
   key = pygame.key.get_pressed()
