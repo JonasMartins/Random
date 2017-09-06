2a700220596cd4faf29381041f5b00ee0be58e69
JonasMartins <jonas.w.martins@gmail.com>
2017-09-06 13:12:42 -0300

    another tutorial

--- SD/Servidor.java
+++ SD/Servidor.java
@@ -1,23 +1,33 @@
 import java.io.IOException;
- import java.net.ServerSocket;
- import java.net.Socket;
- import java.util.Scanner;
- 
- public class Servidor {
-   public static void main(String[] args) throws IOException {
-     ServerSocket servidor = new ServerSocket(12345);
-     System.out.println("Porta 12345 aberta!");
-     
-     Socket cliente = servidor.accept();
-     System.out.println("Nova conexão com o cliente " +   
-       cliente.getInetAddress().getHostAddress());
-     
-     Scanner entrada = new Scanner(cliente.getInputStream());
-     while (entrada.hasNextLine()) {
-       System.out.println(entrada.nextLine());
-     }
-     
-     entrada.close();
-     servidor.close();
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class Servidor {
+ public static void main(String[] args) throws IOException
+ {
+   ServerSocket servidor = new ServerSocket(12345);
+   
+   /** O servidor espera por 3 segundos por uma mensagem 
+   do cliente */
+   try {
+    servidor.setSoTimeout(3);
+   } 
+   catch(SocketException e){
+    e.printStackTrace();
    }
+   System.out.println("Porta 12345 aberta!");
+   
+   Socket cliente = servidor.accept();
+   System.out.println("Nova conexão com o cliente " +   
+     cliente.getInetAddress().getHostAddress());
+   
+    Scanner entrada = new Scanner(cliente.getInputStream());
+    while (entrada.hasNextLine()) {
+     System.out.println(entrada.nextLine());
+   }
+   
+   entrada.close();
+   servidor.close();
  }
+}
