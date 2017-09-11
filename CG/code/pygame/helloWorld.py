fa9431fbfea623cb8e9e96b868d563cff5c3e3c8
JonasMartins <jonas.s.martins@hotmail.com>
2017-09-11 20:28:04 -0300

    pygame 03

--- CG/code/pygame/helloWorld.py
+++ CG/code/pygame/helloWorld.py
@@ -2,7 +2,25 @@ import pygame
 
 pygame.init()
 
-gameDysplay = pygame.display.set_mode((800,600))
+display_width = 800
+display_height = 600
+
+black = (0,0,0)
+white = (255,255,255)
+red = (255,0,0)
+
+carImg = pygame.image.load('car_72.png')
+
+def car(x,y):
+  gameDysplay.blit(carImg, (x,y))
+
+
+x = (display_width * 0.45)
+y = (display_height * 0.8)
+
+x_change = 0
+
+gameDysplay = pygame.display.set_mode((display_width,display_height))
 
 # window title
 pygame.display.set_caption('A bit Racy')
@@ -19,8 +37,20 @@ while not crashed:
     if event.type == pygame.QUIT:
       crashed = True
 
-    print(event)
+    if event.type == pygame.KEYDOWN:
+      if event.key == pygame.K_LEFT:
+        x_change = -15
+      if event.key == pygame.K_RIGHT:
+        x_change = 15
 
+    if event.type == pygame.KEYUP:
+      if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+        x_change = 0
+
+  x += x_change
+        
+  gameDysplay.fill(white)
+  car(x,y)
   pygame.display.update()
 
   # frames per second
