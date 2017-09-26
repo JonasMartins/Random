29592c0effed013f879a1a0cd026547cf3da0e3f
JonasMartins <jonas.s.martins@hotmail.com>
2017-09-25 21:43:16 -0300

    Flexibilizando matrix_opearaions

--- CG/implentações_para_prova/cisalhamento.py
+++ CG/implentações_para_prova/cisalhamento.py
@@ -3,14 +3,30 @@
 
 from matrix_operations import Matrix
 
-m1 = Matrix(file = 'matriz1.txt')
-m2 = Matrix(file = 'matriz2.txt')
+# m1 = Matrix(file = 'matriz1.txt')
+# m2 = Matrix(file = 'matriz2.txt')
 
-m1.setEnvFile();
-m2.setEnvFile();
 
-m1.fillMatrix()
-m2.fillMatrix()
+# o length do vetor é o numero de colunas
+# a quantidade de cada elemento é o numero de linhas
+v = [[0,0,7,1]]
 
-m1.showMatrix()
-m2.showMatrix()
+z = [[0,0,7,1],[0,0,7,1]]
+
+# m3 = Matrix(rows = 4,columns = 4)
+
+m4 = Matrix(matrix = v)
+m4.showMatrix()
+
+# m1.setEnvFile();
+# m2.setEnvFile();
+
+# m1.fillMatrix()
+# m2.fillMatrix()
+
+# m1.showMatrix()
+# m2.showMatrix()
+# m3.generate_identity()
+# m3.showMatrix()
+# 
+
--- CG/implentações_para_prova/matrix_operations.py
+++ CG/implentações_para_prova/matrix_operations.py
@@ -5,12 +5,31 @@ import sys
 
 class Matrix:    
 
-  """ ULTRA FLEXIBLE PYTHON CLASS, ACCEPT FILE MATRIXES AND 
-  NORMAL DEFINICIONS """
+  """ 
+  ULTRA FLEXIBLE PYTHON CLASS, ACEITA 3 TIPOS DE DEFINIÇÕES DE OBJETOS, 
+  UMA MATRIZ VINDA DE UM ARQUIVO, PASSADOS APENAS AS LINHAS A COLUNAS, E
+  A MATRIZ PASSA EM SÍ, COMO OM VETOR MULTIDIMENCIONAL, O CONSTRUTOR SE ENCARREGA
+  DE IDENTIFICAR QUAL TIPO DE OBJETO FOI CIRADO ATRAVÉS DO **kwargs
+
+  """
   
   def __init__(self, **kwargs):
     self.variables = kwargs
+    
+    # para pegar uma matriz de um arquivo texto
+    if(self.variables.has_key('file')):
+      self.__setEnvFile()
+    
+    # para pegar de um vetor escrito no proprio codigo
+    if(self.variables.has_key('matrix')):
+      # m = 
+      self.set_variable('columns',len(self.get_variable('matrix')))
+      self.set_variable('rows',len(self.get_variable('matrix')[0]))    
 
+    # para pegar de uma matrix já especificada com rows e columns
+    # ela já inicia toda igual a zero 
+    else:
+      self.__allocMatrix()
 
   def set_variable(self,k,v):
     self.variables[k] = v
@@ -18,30 +37,34 @@ class Matrix:
   def get_variable(self,k):
     return self.variables.get(k,None)
     
-  def setEnvFile(self):
-    self.set_variable('columns',self.getColumns())
-    self.set_variable('rows',self.getRows())
-    self.allocMatrix()
+  # setando o ambiente para matrizes vindas de arquivos texto
+  def __setEnvFile(self):
+
+    self.set_variable('columns',self.__getColumns())
+    self.set_variable('rows',self.__getRows())
+    self.__allocMatrix()
+
+  # apenas printando as dimensões da matriz, com proṕosito de debug 
+  def show_dimensions(self):
+    print('{0} x {1}'.format(self.get_variable('rows'),self.get_variable('columns')))
 
   # Percorre a matriz do arquivo e cada item Ã© armazenado na matriz da memÃ³ria
   def fillMatrix(self):
     h = w = 0
-    mm = self.get_variable('matrix')
     with open(self.get_variable('file')) as m:
        for line in m:
           line = line.split(',')
           line[len(line)-1] = line[len(line)-1].replace("\n","") 
           for i in range(len(line)):
-            mm[h][w] = int(line[i])
+            self.get_variable('matrix')[h][w] = int(line[i])
             w += 1
           h += 1 
           w = 0
           if 'str' in line:
             break
-    self.set_variable('matrix',mm)
 
   # Retona a dimensão de colunas de uma matriz
-  def getColumns(self):
+  def __getColumns(self):
     length = 0
     with open(self.get_variable('file')) as m1:
       s = m1.readline()
@@ -49,12 +72,12 @@ class Matrix:
     return len(array) # {\n}
 
   # Retona a dimensÃ£o de linhas de uma matriz
-  def getRows(self):
+  def __getRows(self):
     with open(self.get_variable('file')) as f:
       return sum(1 for _ in f)
 
   # Checar a disponibilidade ao se abrir um arquivo
-  def checkOpen(self):
+  def __checkOpen(self):
     value = False
     try:
       m1 = open(self.get_variable('file'))
@@ -70,22 +93,25 @@ class Matrix:
       raise
     return value
 
+  # Transformando a matriz do objeto em uma matriz identidade, caso seja axa
+  def generate_identity(self):
+    if(self.get_variable('rows') == self.get_variable('columns')):
+      for i in range(0,self.get_variable('rows')):
+        self.get_variable('matrix')[i][i] = 1
 
   # alocando a matriz na memoria
-  def allocMatrix(self): 
+  def __allocMatrix(self): 
     m = [[0 for x in range(self.get_variable('rows'))] for y in range(self.get_variable('columns'))]
     self.set_variable('matrix',m)  
 
   def showMatrix(self):
-    r = self.get_variable('rows')
-    c = self.get_variable('columns')
-    m = self.get_variable('matrix')
-    for i in range(0,r):
-      for j in range(0,c):
-        sys.stdout.write(str(m[i][j]))
+    print('\n********************')
+    for i in range(0,self.get_variable('columns')):
+      for j in range(0,self.get_variable('rows')):
+        sys.stdout.write(str(self.get_variable('matrix')[i][j]))
         sys.stdout.write('-') 
       print('')
-    print('=======================')
+    print('\n********************')
     return
 
 
