48b7cf875a3bd3435bbe2c9e0d512f3ccbc44d9b
JonasMartins <jonas.w.martins@gmail.com>
2017-09-27 10:51:28 -0300

    Cisalhamento inicial

--- CG/implentações_para_prova/cisalhamento.py
+++ CG/implentações_para_prova/cisalhamento.py
@@ -1,8 +1,57 @@
 #!/usr/bin/python
 # encoding: utf-8
 
-import sys
+import math
 from matrix_operations import Matrix
+
+class Cisalhamento:
+
+  def __init__(self,alfa):
+    pass
+
+  cyx = [ [1,0,0,0],
+          [0,1,0,0],
+          [0,0,1,0],
+          [0,0,0,1] ]
+  cyz = [ [1,0,0,0],
+          [0,1,0,0],
+          [0,0,1,0],
+          [0,0,0,1] ]
+
+  cxz= [ [1,0,0,0],
+          [0,1,0,0],
+          [0,0,1,0],
+          [0,0,0,1] ]
+
+  czx = [ [1,0,0,0],
+          [0,1,0,0],
+          [0,0,1,0],
+          [0,0,0,1] ]
+  czy = [ [1,0,0,0],
+          [0,1,0,0],
+          [0,0,1,0],
+          [0,0,0,1] ]
+
+  cxy = [ [1,0,0,0],
+          [0,1,0,0],
+          [0,0,1,0],
+          [0,0,0,1] ]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 # m1 = Matrix(file = 'matriz1.txt')
 # m2 = Matrix(file = 'matriz2.txt')
@@ -10,7 +59,7 @@ from matrix_operations import Matrix
 
 # o length do vetor é o numero de colunas
 # a quantidade de cada elemento é o numero de linhas
-v = [[0,0,7,1]]
+# v = [[0,0,7,1]]
 
 # m3 = Matrix(rows = 4,columns = 4)
 
@@ -21,41 +70,42 @@ v = [[0,0,7,1]]
 # m3.showMatrix()
 # 
 
-def checkTimes(matrix1,matrix2):
-  if matrix1.get_columns() != matrix2.get_rows():
-    print('Impossível multiplicar as matrizes dadas')
-    sys.exit()
-  return
+# def checkTimes(matrix1,matrix2):
+#   if matrix1.get_columns() != matrix2.get_rows():
+#     print('Impossível multiplicar as matrizes dadas')
+#     sys.exit()
+#   return
 
-def timesMetrix(m1,m2,scalar):
-  m3 = None
-  if scalar is not None:
-    m3 = Matrix(rows = m1.get_rows(),columns = m1.get_columns())
-    for i in range(0,m3.get_variable('columns')):
-      for j in range(0,m3.get_variable('rows')): 
-        m3.get_variable('matrix')[i][j] = m1.get_variable('matrix')[i][j] * scalar
+# def timesMetrix(m1,m2,scalar):
+#   m3 = None
+#   if scalar is not None:
+#     m3 = Matrix(rows = m1.get_rows(),columns = m1.get_columns())
+#     for i in range(0,m3.get_variable('columns')):
+#       for j in range(0,m3.get_variable('rows')): 
+#         m3.get_variable('matrix')[i][j] = m1.get_variable('matrix')[i][j] * scalar
 
-  else:
-    checkTimes(m1,m2)
+#   else:
+#     checkTimes(m1,m2)
     
-    h = m1.get_rows()
-    w = m2.get_columns()
-    y = m2.get_rows()
+#     h = m1.get_rows()
+#     w = m2.get_columns()
+#     y = m2.get_rows()
 
-    m3 = Matrix(rows = h,columns = w)
-    for i in range(0,w):
-      for j in range(0,h):
-        for k in range(0,y):
-          m3.get_variable('matrix')[i][j] += m1.get_variable('matrix')[i][k] * m2.get_variable('matrix')[k][j]
+#     m3 = Matrix(rows = h,columns = w)
+#     for i in range(0,w):
+#       for j in range(0,h):
+#         for k in range(0,y):
+#           m3.get_variable('matrix')[i][j] += m1.get_variable('matrix')[i][k] * m2.get_variable('matrix')[k][j]
 
-  return m3
+#   return m3
 
-def main():
-  # m3 = Matrix(file='matriz1.txt')
-  # m4 = Matrix(file='matriz1.txt')
-  m5 = Matrix(matrix = v)
-  m7 = timesMetrix(m5,None,5)
-  m7.showMatrix()
-  
+# def main():
+#   m3 = Matrix(file='matriz1.txt')
+#   # m4 = Matrix(file='matriz1.txt')
+#   m5 = Matrix(matrix = v)
+#   # m7 = timesMetrix(m5,None,5)
+#   # m7.showMatrix()
+#   t = Transformation()
+#   t.set_variable('matrix1',m5)
 
 if __name__ == "__main__": main()
--- CG/implentações_para_prova/transformations.py
+++ CG/implentações_para_prova/transformations.py
@@ -6,33 +6,35 @@ from matrix_operations import Matrix
 
 class Transformation:
   
-  def __init__(self,m1,m2):
-    m1.__class__ = Matrix
-    m2.__class__ = Matrix
-    
-    self.matrix1 = m1 #Matrix(matrix = m1)
-    self.matrix2 = m2 # Matrix(matrix = m2)
-    self.__checkTimes()
+  def __init__(self, **kwargs):
+    self.variables = kwargs
+
+  def set_variable(self,k,v):
+    self.variables[k] = v
+
+  def get_variable(self,k):
+    return self.variables.get(k,None)
+   
 
   # # checar possibilidade de multipliçãoo entre as matrizes
-  def __checkTimes(self):
-    if(self.matrix1.get_columns() != self.matrix2.get_rows()):
-      print('Impossível multiplicar as matrizes dadas')
-    return
+  # def __checkTimes(self):
+  #   if(self.matrix1.get_columns() != self.matrix2.get_rows()):
+  #     print('Impossível multiplicar as matrizes dadas')
+  #   return
 
-  def timesMetrix(self):
+  # def timesMetrix(self):
 
-    h = self.matrix1.get_rows()
-    w = self.matrix2.get_columns()
-    y = self.matrix2.get_rows()
+  #   h = self.matrix1.get_rows()
+  #   w = self.matrix2.get_columns()
+  #   y = self.matrix2.get_rows()
 
-    self.m3 = Matrix(h,w)
+  #   self.m3 = Matrix(h,w)
 
-    for i in range(0,h):
-      for j in range(0,w):
-        for k in range(0,y):
-          m3[i][j] += m1[i][k] * m2[k][j]
+  #   for i in range(0,h):
+  #     for j in range(0,w):
+  #       for k in range(0,y):
+  #         m3[i][j] += m1[i][k] * m2[k][j]
 
-    return m3
+  #   return m3
 
     
