eaf959c4681cf622bdffa67e21f653272ad68730
JonasMartins <jonas.s.martins@hotmail.com>
2017-10-12 16:15:45 -0300

    quaternio part2

--- CG/implentações_para_prova/main.py
+++ CG/implentações_para_prova/main.py
@@ -22,7 +22,7 @@ def quaternion(v,theta):
     (vv[0][2] * math.sin(t))]]) 
   return q
 
-# retorna o inverso de um quaternio
+# # retorna o inverso de um quaternio
 def quaternion_i(q):
   qq = [[q[0][0],0,0,0]]
   qq[0][1] = q[0][1] * (-1) 
@@ -30,72 +30,54 @@ def quaternion_i(q):
   qq[0][3] = q[0][3] * (-1)
   return qq
 
-def run_quaternion(u,q,q_i):
-  a = np.dot(q,u)
-  return np.dot(q_i,a[0][0])
+def run_quaternion(u,q):
+  x = q[0][0]; y = q[0][1]; z = q[0][2]; w = q[0][3]
+  m = [
+  [ (math.pow(w,2) + math.pow(x,2)) - (math.pow(y,2) - math.pow(z,2)),
+    (2*x*y) - (2*w*z), (2*x*z) + (2*w*y), 0 ],
+  [(2*x*y) + (2*w*z), (math.pow(w,2) - math.pow(x,2)) + (math.pow(y,2) - math.pow(z,2)),
+  (2*y*z) - (2*w*x),0],
+  [(2*x*z) - (2*w*y), (2*y*z) + (2*w*x), (math.pow(w,2) - math.pow(x,2)) - (math.pow(y,2) + math.pow(z,2)),0],
+  [0,0,0,(math.pow(w,2) + math.pow(x,2)) + (math.pow(y,2) + math.pow(z,2))]
+  ]
+  return m
+  # a = np.dot(u,qq)
+  # return np.dot(q,a)
 
 def main():
 
   
-  v_trans = [-1,-1,-1]
+  v_trans = [90,50,0]
   v_scale = [2,2,2]
 
 
   v = np.array([[220,290,0,1]])
   vv = np.array([[-220,-290,0,1]])
-  
-  v = [[1,0,0,-220],
-        [0,1,0,-290],
-        [0,1,0,0],
-        [0,0,0,1]]
-
-  mx = [[-0.9164,0,-0.43664,220],
-  			[0,1,0,0],
-  			[-0.43664,0,0.89974,290],
-  			[0,0,0,1]]
-
+ 
 
   # m = Matrix(matrix = v)
   # m1 = Matrix(matrix = mx)
 
   # cisalhamento = Transformation(30,[0,0,0],[0,0,0],0).get_matrix(0)
-  # translacao = Transformation(0,v_trans,[0,0,0],0).get_matrix(6)
+  translacao = Transformation(0,v_trans,[0,0,0],0).get_matrix(6)
   # scala = Transformation(0,[0,0,0],v_scale,0).get_matrix(7)
   # rotacao = Transformation(0,[0,0,0],[0,0,0],270).get_matrix(9)
   # espelho = Transformation(0,[0,0,0],[0,0,0],0).get_matrix(11)
 
-  axis = np.array([[1,1,1,0]])
-  vector = np.array([[7],[2],[5],[0]])
+  axis = np.array([[7.776,-9.077,-7.569,0]])
+  vector = np.array([[3.836],[11.37],[20.319],[1]])
 
-  q =  quaternion(axis,240)
-  qq = np.array(quaternion_i(q))
-  # print q
-  # print qq
+  # q =  quaternion(axis,90)
 
-  print run_quaternion(vector,q,qq)
+  # vv = vector/la.norm(vector)
 
-  # m = np.array(v) 
-  # n = np.array(espelho)
-
-  # # multiplicação de matrizes numpy
-  # print m
-  # print '----------------------------'
-  # print n
-  # print '----------------------------'
+  # m = run_quaternion(vector,q)
   
-  # # mutiplicar por escalar 
-  # result =  np.dot(m,10)
+  m = np.array(translacao)
+
+  print np.dot(m,vector)
   
+
   
-  # # norma de um vetor 
-  # print 'result: {0} norm: {1}' .format(result,la.norm(result))
-  # normalize(result)
-
-  # normalizando na classe matrix
-  # print r.get_variable('norm')
-  #norma = np.array(r.get_variable('normalized'))
-
-  #print norma
-
 
 if __name__ == "__main__": main()
