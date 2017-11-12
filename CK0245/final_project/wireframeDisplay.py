6a1094f3d9fc30542ac28310f583c0035495776e
JonasMartins <jonas.martins@lsbd.ufc.br>
2017-11-12 17:16:44 -0300

    shade sphere like a beach ball

--- CK0245/final_project/wireframeDisplay.py
+++ CK0245/final_project/wireframeDisplay.py
@@ -51,19 +51,20 @@ class WireframeViewer(wf.WireframeGroup):
         self.perspective = False #300.
         self.eyeX = self.width/2
         self.eyeY = 100
+        self.view_vector = np.array([0, 0, -1])
         
         self.light = wf.Wireframe()
         self.light.addNodes([[0, -1, 0]])
-        self.light.transform(wf.rotateZMatrix(0.3))
-        self.light.transform(wf.rotateXMatrix(0.4))
         
-        self.min_light = 0.05 * 255
-        self.max_light = 0.92 * 255
+        self.min_light = 0.02
+        self.max_light = 1.0
         self.light_range = self.max_light - self.min_light 
         
         self.background = (10,10,50)
         self.nodeColour = (250,250,250)
         self.nodeRadius = 4
+        
+        self.control = 0
     
     def addWireframe(self, name, wireframe):
         self.wireframes[name] = wireframe
@@ -98,25 +99,32 @@ class WireframeViewer(wf.WireframeGroup):
 
     def display(self):
         self.screen.fill(self.background)
+        light = self.light.nodes[0][:3]
+        spectral_highlight = self.light.nodes[0][:3] + self.view_vector
+        spectral_highlight /= np.linalg.norm(spectral_highlight)
         
         for name, wireframe in self.wireframes.items():
-            colour = self.wireframe_colours.get(name)
             nodes = wireframe.nodes
-            light = self.light.nodes[0][:3]
             
-            if colour:
-                if self.displayFaces:
-                    for face in wireframe.sortedFaces():
-                        v1 = (nodes[face[1]] - nodes[face[0]])[:3]
-                        v2 = (nodes[face[2]] - nodes[face[0]])[:3]
-                        normal = np.cross(v1, v2)   # test to see whether it faces us
+            if self.displayFaces:
+                for (face, colour) in wireframe.sortedFaces():
+                    v1 = (nodes[face[1]] - nodes[face[0]])[:3]
+                    v2 = (nodes[face[2]] - nodes[face[0]])[:3]
+                    
+                    normal = np.cross(v1, v2)
+                    towards_us = np.dot(normal, self.view_vector)
+                    
+                    # Only draw faces that face us
+                    if towards_us > 0:
                         normal /= np.linalg.norm(normal)
-                        
                         theta = np.dot(normal, light)
+                        #catchlight_face = np.dot(normal, spectral_highlight) ** 25
+
+                        c = 0
                         if theta < 0:
-                            shade = (self.min_light, 0, 0)
+                            shade = self.min_light * colour
                         else:
-                            shade = (int(theta*self.light_range+self.min_light), 0, 0)
+                            shade = (theta * self.light_range + self.min_light) * colour
                         pygame.draw.polygon(self.screen, shade, [(nodes[node][0], nodes[node][1]) for node in face], 0)
                         
                         #mean_x = sum(nodes[node][0] for node in face) / len(face)
@@ -147,8 +155,8 @@ class WireframeViewer(wf.WireframeGroup):
 
     def keyEvent(self, key):
         if key in key_to_function:
-            #key_to_function[key](self)
-            light_movement[key](self.light)
+            key_to_function[key](self)
+            #light_movement[key](self.light)
 
     def run(self):
         """ Display wireframe on screen and respond to keydown events """
