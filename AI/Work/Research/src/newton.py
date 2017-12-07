576098117634c8b118b1c8e4428f3858e06ee422
JonasMartins <jonas.martins@lsbd.ufc.br>
2017-12-07 11:35:52 -0300

    IA update

--- AI/Work/Research/src/newton.py
+++ AI/Work/Research/src/newton.py
@@ -19,13 +19,30 @@ from line_search import find_step_length
 # fd gradiente de f
 # fdd hassian matrix
 # callback uma funcao para imprimir cada iteracao
-
 def newton(f, fd, fdd, x, max_iterations, precision, callback):
 	for i in range(1, max_iterations):
 		gradient = fd(x)
 		hessian = fdd(x)
 		
+		# linalg: numpy function
+		# linalg.solve: Solve a linear matrix equation, or system of linear scalar equations.
+		# Computes the “exact” solution, x, of the well-determined, i.e., full rank, linear matrix equation ax = b.
 		direction = -linalg.solve(hessian, gradient)
+		
+		'''
+		Example:
+		3 * x0 + x1 = 9 and x0 + 2 * x1 = 8:
+		a = np.array([[3,1], [1,2]])
+		b = np.array([9,8])
+		x = np.linalg.solve(a, b)
+		x: array([ 2.,  3.])
+		'''
+		
+		# Metodo tradicional:
+		# ainv is the hassian inverse
+		# ainv = linalg.inv(hessian)
+		
+
 		alpha = find_step_length(f, fd, x, 1.0, direction, c2=0.9)
 		x_prev = x
 		x = x + alpha*direction
