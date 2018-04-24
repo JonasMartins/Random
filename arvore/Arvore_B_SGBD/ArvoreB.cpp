3e8f5cf43d48cb4db6832c6d1b0e01c4af6c365c
JonasMartins <jonas.w.martins@gmail.com>
2018-04-24 18:29:55 -0300

    ab+ pt5

--- arvore/Arvore_B_SGBD/ArvoreB.cpp
+++ arvore/Arvore_B_SGBD/ArvoreB.cpp
@@ -16,26 +16,26 @@ char * query_bpt( int key );
 
 void start()
 {
-    root = new_node() ;
-    root->is_root = true ;
-    root->is_leaf = true ;
-    node_count = 0 ;
-    splite_count = 0 ;
+  root = new_node() ;
+  root->is_root = true ;
+  root->is_leaf = true ;
+  node_count = 0 ;
+  splite_count = 0 ;
 
-    cout<<"Start build first node, the Tree Root"<<endl;
+  cout<<"Start build first node, the Tree Root"<<endl;
 }
 
 node * new_node()
 {
-    node_count ++ ;
+  node_count ++ ;
 
-    node *p = new node ;
-    p->is_leaf = false ;
-    p->is_root = false ;
-    p->key_num = 0 ;
-    p->pointer[ 0 ] = NULL ;
-    p->father = NULL ;
-    return p ;
+  node *p = new node ;
+  p->is_leaf = false ;
+  p->is_root = false ;
+  p->key_num = 0 ;
+  p->pointer[ 0 ] = NULL ;
+  p->father = NULL ;
+  return p ;
 }
 
 node *find_leaf( int key )
@@ -43,12 +43,12 @@ node *find_leaf( int key )
    node *now = root ;
    while ( !now->is_leaf )
    {
-      for ( int i = 0 ; i <= now->key_num ; i ++ )
-        if ( i == now->key_num || key < now->key[ i ] )
-        {
-            now = ( node *) now->pointer[ i ] ;
-            break ;
-        }
+    for ( int i = 0 ; i <= now->key_num ; i ++ )
+      if ( i == now->key_num || key < now->key[ i ] )
+      {
+        now = ( node *) now->pointer[ i ] ;
+        break ;
+      }
   }
   return now ;
 }
@@ -56,75 +56,85 @@ node *find_leaf( int key )
 
 void split( node *_node )
 {   
-    splite_count ++ ;
-    
-    cout << endl << "split!:" << _node->key_num << ' ' << _node->key[ 0 ] << ' ' << _node->key[ 1 ] << ' ' << _node->key[ 2 ] << endl;
-    
-    nodd = new_node() ;
-    int mid_key = _node->key[ M / 2 ] ;
+  splite_count ++ ;
+  cout << endl << "split!:" << _node->key_num << ' ' << _node->key[ 0 ] << ' ' << _node->key[ 1 ] << ' ' << _node->key[ 2 ] << endl;
+  nodd = new_node() ;
 
-    nodd->key_num = M - M / 2 - 1 ;
-    for ( int i = 0 ; i < nodd->key_num ; i ++ )
-    {
-        nodd->key[ i ] = _node->key[ i + ( M / 2 + 1 ) ] ;
-        nodd->pointer[ i ] = _node->pointer[ i + ( M / 2 + 1 ) ] ;
-    }
-    nodd->pointer[ nodd->key_num ] = _node->pointer[ M ] ;
-    _node->key_num = M / 2 ;
+  int mid_key = _node->key[ M / 2 ] ;
 
-    if ( _node -> is_leaf )
-    {
-        _node->key_num ++ ;
-        nodd->pointer[ 0 ] = _node->pointer[ 0 ] ;
-        _node->pointer[ 0 ] = nodd ;
-        nodd->is_leaf = true ;
-        mid_key = _node->key[ M / 2 + 1 ] ;
-    }
+  nodd->key_num = M - M / 2 - 1 ;
+  for ( int i = 0 ; i < nodd->key_num ; i ++ )
+  {
+    nodd->key[ i ] = _node->key[ i + ( M / 2 + 1 ) ] ;
+    nodd->pointer[ i ] = _node->pointer[ i + ( M / 2 + 1 ) ] ;
+  }
+  nodd->pointer[ nodd->key_num ] = _node->pointer[ M ] ;
+  _node->key_num = M / 2 ;
 
-    if ( _node -> is_root )
-    {
-        _node-> is_root = false ;
-        root = new_node() ;
-        root->is_root = true ;
-        root->key[ 0 ] = mid_key ;
-        root->pointer[ 0 ] = _node ;
-        root->pointer[ 1 ] = nodd ;
-        root->key_num = 1 ;
+  if ( _node -> is_leaf )
+  {
+    _node->key_num ++ ;
+    nodd->pointer[ 0 ] = _node->pointer[ 0 ] ;
+    _node->pointer[ 0 ] = nodd ;
+    nodd->is_leaf = true ;
+    mid_key = _node->key[ M / 2 + 1 ] ;
+  }
 
-        _node->father = nodd->father = root ;
-    } else
-    {
-        nodd->father = _node->father;
-        insert_node( (node*)_node->father,mid_key,(void *)nodd ) ;
-    }
-    
-    cout << "midnum: " << mid_key << endl;
-    cout << "father: " << (_node->father )<< ' ' << _node->key_num << ' '<< (_node -> father )<< ' ' << _node->key[ 0 ] << endl;
-    cout << "node: " << _node->key_num << ' ' << _node -> key[ 0 ] << ' ' << _node -> key[ 1 ] << endl;
-    cout << "nodd: " << nodd->key_num << ' ' << nodd -> key[ 0 ] << ' ' << nodd -> key[ 1 ] << endl << endl ;
+  if ( _node -> is_root )
+  {
+    _node-> is_root = false ;
+    root = new_node() ;
+    root->is_root = true ;
+    root->key[ 0 ] = mid_key ;
+    root->pointer[ 0 ] = _node ;
+    root->pointer[ 1 ] = nodd ;
+    root->key_num = 1 ;
+
+    _node->father = nodd->father = root ;
+  } else
+  {
+    nodd->father = _node->father;
+    insert_node( (node*)_node->father,mid_key,(void *)nodd ) ;
+  }
+  
+  cout << "midnum: " << mid_key << endl;
+  cout << "father: " << (_node->father )<< ' ' << _node->key_num << ' '<< (_node -> father )<< ' ' << _node->key[ 0 ] << endl;
+  cout << "node: " << _node->key_num << ' ' << _node -> key[ 0 ] << ' ' << _node -> key[ 1 ] << endl;
+  cout << "nodd: " << nodd->key_num << ' ' << nodd -> key[ 0 ] << ' ' << nodd -> key[ 1 ] << endl << endl ;
     
 }
 
 void insert_node( node *node , int key , void *value )
 {   
-    char * buffer;
-    int x = 0 ;
-    while ( x < node -> key_num && node -> key[ x ] < key ) x ++ ;
-    for ( int i = node -> key_num ; i > x ; i -- )
-        node -> key[ i ] = node -> key[ i - 1 ] ;
-    for ( int i = node -> key_num + 1 ; i > x + 1 ; i -- )
-        node -> pointer[ i ] = node -> pointer[ i - 1 ] ;
-    node -> key[ x ] = key ;
-    node -> pointer[ x + 1 ] = value ;
-    cout << "x: "<< x <<endl;
-    buffer = (char *)node->pointer[ x + 1 ]; 
+  char * buffer;
+  int x = 0 ;
+  while (x < node->key_num && node->key[x]<key) x ++;
+  for (int i=node->key_num; i>x; i--)
+    node->key[i] = node->key[i-1];
+  for (int i=node->key_num+1; i>x+1; i--)
+    node->pointer[i] = node->pointer[i-1];
+  node->key[ x ] = key ;
+  node->pointer[ x + 1 ] = value ;
+  cout << "x: "<< x <<endl;
+  //buffer = (char *)node->pointer[x+1]; 
 
-    printf("%s%s\n","pointer x+1: ",buffer);
-    node -> key_num ++ ;
+  // printf("%s%s\n","pointer x+1: ",buffer);
+  for(int j=0;j<=x+1;j++){
+    try {
+      buffer = (char *)node->pointer[j];
+      printf("%s%d%s%s\n","pointer: ",j," -> ",buffer);
+    }catch(const std::exception &exc){
+      std::cerr << exc.what(); 
+    }
+  }
 
-    printNode(node);
-    if ( node -> key_num == M ) // split
-        split( node ) ;
+  //cout <<"pointer_with_x: "<< x+1 << node->pointer[x+1]<<endl;
+  //printf("%s%s\n","pointer_with_x: ",node->pointer[x+1] );
+  node -> key_num ++ ;
+  printNode(node);
+  //cout << "TYPE: "<< typeid(node->pointer[x+1]).name() << endl;
+  if ( node -> key_num == M ) // split
+      split( node ) ;
 }
 
 void delete_node(node *node , int key )
@@ -169,10 +179,11 @@ char * query_bpt( int key )
     return NULL ;
 }
 
-void printNode(node *node){
-    printf("Node info:\n");
-    printf("%s%d\n","is_leaf: ",node->is_leaf );
-    printf("%s%d\n","is_root: ",node->is_root );
-    printf("%s%d\n","key_num: ",node->key_num );
-    //cout << "TYPE: "<< typeid(node->pointer[4]).name() << endl;
+void printNode(node *node)
+{
+  printf("Node info:\n");
+  printf("%s%d\n","is_leaf: ",node->is_leaf );
+  printf("%s%d\n","is_root: ",node->is_root );
+  printf("%s%d\n","key_num: ",node->key_num );
+  //cout << "TYPE: "<< typeid(node->pointer[4]).name() << endl;
 }
--- arvore/Arvore_B_SGBD/ArvoreB.h
+++ arvore/Arvore_B_SGBD/ArvoreB.h
@@ -1,33 +1,31 @@
 #ifndef __GP__BPT__
 #define __GP__BPT__
 
-#include <iostream>
-#include <string>
-#include <vector>
+#include <iostream> 
+#include <string> 
+#include <vector> 
 #include <typeinfo>
 
-const int M = 4 ; // comprimento do no 
+  const int M = 4; // comprimento do no 
 
-struct node
-{
-    bool is_leaf ;
-    bool is_root ;
-    int key_num ;
-    int key[ M ] ;
-    void *pointer[ M + 1 ] ;
-    void *father ;
-} ;
+struct node {
+  bool is_leaf;
+  bool is_root;
+  int key_num;
+  int key[M];
+  void * pointer[M + 1];
+  void * father;
+};
 
 node * new_node();
-void splite(node *node );
-void printNode(node *node );
+void splite(node * node);
+void printNode(node * node);
 
-extern int node_count ;
-extern int splite_count ;
-extern void start() ;
-extern bool insert_bpt( int key , void *value ) ;
-extern bool delete_bpt( int key ) ;
-extern char *query_bpt( int key ) ;
-
+extern int node_count;
+extern int splite_count;
+extern void start();
+extern bool insert_bpt(int key, void * value);
+extern bool delete_bpt(int key);
+extern char * query_bpt(int key);
 
 #endif
