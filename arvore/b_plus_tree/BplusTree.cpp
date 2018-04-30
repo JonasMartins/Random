f7086fb8f69aa8dc60b537f7901b10dca920fafe
JonasMartins <jonas.w.martins@gmail.com>
2018-04-30 16:18:47 -0300

    ab+ pt11

--- arvore/b_plus_tree/BplusTree.cpp
+++ arvore/b_plus_tree/BplusTree.cpp
@@ -22,7 +22,7 @@ void BplusTree::run(int n)
   getchar();
 }
 
-
+// iniciando o nó raíz para a inserção
 node * BplusTree::init()
 {
   int i;
@@ -37,11 +37,14 @@ node * BplusTree::init()
   }
   return np;
 }
-
+// Recebendo um nó como argumento, é usada apenas uma vez por ser
+// recursiva, pegando apenas o nó root, e chamando a si mesma para
+// passear pelas subárvores
 void BplusTree::traverse(node *p)
 {
   cout<<endl;
   int i;
+  // n o número de posições preenchidas daquele nó
   for (i = 0; i < p->n; i++)
   {
     if (p->leaf == false)
@@ -56,7 +59,10 @@ void BplusTree::traverse(node *p)
   }
   cout<<endl;
 }
-
+// Ordenando apenas os valores dentro do array de inteiros
+// data que ficam em cada nó, penultima linha da inserção
+// logo antes de incrementar a variável que conta os espaços
+// preenchidos de cada nó.
 void BplusTree::sort(int *p, int n)
 {
   int i, j, temp;
@@ -66,9 +72,9 @@ void BplusTree::sort(int *p, int n)
     {
       if (p[i] > p[j])
       {
-          temp = p[i];
-          p[i] = p[j];
-          p[j] = temp;
+        temp = p[i];
+        p[i] = p[j];
+        p[j] = temp;
       }
     }
   }
@@ -80,14 +86,21 @@ int BplusTree::split_child(node *x, int i)
   node *np1, *np3, *y;
   np3 = init();
   np3->leaf = true;
+  // i = -i indica que esse nó é folha e não tem mais espaços vazios
+  //
   if (i == -1)
   {
+    // valor do meio que vai subir depois do split
+    // fixo para ser 2, mudar o degree vai dar erro
     mid = x->data[2];
-    x->data[2] = 0;
-    x->n--;
-    np1 = init();
+    x->data[2] = 0; // esse valor sai do no a sofre split
+    x->n--; // o valor de seus espaços preenchidos é decremntado
+    np1 = init(); // iniciando nó auxiliar como raíz
     np1->leaf = false;
-    x->leaf = true;
+    x->leaf = true;// nó a sofrer split é identificado como folha
+    
+    // preenchndo o nó da direita, com valores
+    // maiores ao nó que foi definido como mid
     for (j = 3; j < degree; j++)
     {
         np3->data[j - 3] = x->data[j];
@@ -96,6 +109,8 @@ int BplusTree::split_child(node *x, int i)
         x->data[j] = 0;
         x->n--;
     }
+    // preenchndo o nó da esquerda, com valores
+    // menores ao nó que foi definido como mid
     for(j = 0; j < degree+1; j++)
     {
         x->child_ptr[j] = NULL;
@@ -106,7 +121,9 @@ int BplusTree::split_child(node *x, int i)
     np1->n++;
     root = np1;
   }
-  else
+  // entrada não é -1 indicando que se o 
+  // filho desse nó não tem mais espaços vazios
+  else 
   {
     y = x->child_ptr[i];
     mid = y->data[2];
@@ -122,7 +139,7 @@ int BplusTree::split_child(node *x, int i)
     x->child_ptr[i + 1] = y;
     x->child_ptr[i + 1] = np3; 
   }
-  return mid;
+  return mid; // retorna o mid desse nó
 }
 
 void BplusTree::insert(int a)
@@ -140,9 +157,9 @@ void BplusTree::insert(int a)
   {
     if (x->leaf == true && x->n == degree) // se é folha e não tem mais espaços vazios
     {
-    cout<<"é folha e não tem mais espaços vazios\n";
+      cout<<"é folha e não tem mais espaços vazios\n";
       temp = split_child(x, -1); // split nesse nó, temp é um int
-      cout<<"temp1: "<<temp<<endl;
+      cout<<"temp1: "<<temp<<"arg: -1"<<endl;
       x = root; // x continua sendo o root
 			cout<<"espaços preenchidos nesse nó: "<<x->n<<endl;
       for (i = 0; i < (x->n); i++) // for pelos espaços preenchidos desse nó 
@@ -151,16 +168,22 @@ void BplusTree::insert(int a)
       	// e menor do que a próxima, incrementa i
         if ((a > x->data[i]) && (a < x->data[i + 1]))
         {
-        		cout<<"enter 2 if\n";
+        		//cout<<"enter 2 if\n";
             i++;
             break;
         }
         // se o valor a ser adicionado for menor que o primeiro elemento do nó, 
         // break e pego o i
         else if (a < x->data[0])
-        { cout<<"enter 3 if\n"; break; }
+        { 
+          //cout<<"enter 3 if\n"; 
+          break; 
+        }
         else
-        { cout<<"enter 4 if\n"; continue; }
+        { 
+          //cout<<"enter 4 if\n"; 
+          continue; 
+        }
       }
       x = x->child_ptr[i]; // desço para o filho com valor i
     	cout<<"i: "<<i<<endl;
@@ -170,36 +193,49 @@ void BplusTree::insert(int a)
     	cout<<"se não é folha ou tem espaços vazios no nó\n";
       while (x->leaf == false) // enquanto esse nó não for folha, 
       {
-      for (i = 0; i < (x->n); i++) // for pelos espaços preenchidos desse nó
-      {	
-      	// se o valor a ser adicionado é maior do que a posição atual
-      	// e menor do que a próxima, incrementa i
-        if ((a > x->data[i]) && (a < x->data[i + 1]))
-        {
-        	cout<<"enter 5 if\n";
-          i++;
-          break;
+        for (i = 0; i < (x->n); i++) // for pelos espaços preenchidos desse nó
+        {	
+        	// se o valor a ser adicionado é maior do que a posição atual
+        	// e menor do que a próxima, incrementa i
+          if ((a > x->data[i]) && (a < x->data[i + 1]))
+          {
+          	//cout<<"enter 5 if\n";
+            i++;
+            break;
+          }
+          // se o valor a ser adicionado for menor que o primeiro elemento do nó, 
+          // break e pego o i
+          else if (a < x->data[0])
+          { 
+            //cout<<"enter 6 if\n";
+            break;
+          }
+          else
+          { 
+            //cout<<"enter 7 if\n";
+            continue; 
+          }
         }
-        // se o valor a ser adicionado for menor que o primeiro elemento do nó, 
-        // break e pego o i
-        else if (a < x->data[0])
-        { cout<<"enter 6 if\n"; break; }
-        else
-        { cout<<"enter 7 if\n"; continue; }
-      }
       	// se o filho desse nó não tiver mais espaços vazios
         if ((x->child_ptr[i])->n == degree)
         {
+          // se o filho estiver sem campos com inteiros para
+          // ser preenchido, chamo o split e passo o i refenrete
+          // ao filho que precisa ser dividido
           temp = split_child(x, i);
-         	cout<<"temp2: "<<temp<<endl;
-          x->data[x->n] = temp;
+         	cout<<"temp2: "<<temp<<"arg: "<<i<<endl;
+          x->data[x->n] = temp; // atualizo a saida do split
           x->n++;
           continue;
         }
         else
         {
-        	cout<<"enter 8 if\n";
+        	//cout<<"enter 8 if\n";
           x = x->child_ptr[i];
+          // atualizo x para ser um de seus filhos 
+          // para o algoritimo continuar descendo até
+          // que a instrução do while seja satisfeita e
+          // x seja folha
         }
       }
     }
