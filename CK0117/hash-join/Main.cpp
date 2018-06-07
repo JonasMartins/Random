c978bfb97f5a9b310fecf85550bdcf05b007b61a
JonasMartins <jonas.w.martins@gmail.com>
2018-06-06 21:45:50 -0300

    Lendo tabelas .dat completas

--- CK0117/hash-join/Main.cpp
+++ CK0117/hash-join/Main.cpp
@@ -4,6 +4,7 @@
 #include<ctime>
 #include<string>
 #include<map>
+#include<ctime>
 #include"./HashJoin.h"
 
 
@@ -37,52 +38,62 @@ int main(int argc, char **argv)
 */
 
 	// esquema para ler linha por linha via fread
+	int start_s=clock();
 
-	// FILE * arq;
-	// arq = fopen("lineitem.txt","r+");
-	// //long lSize;
-	// fpos_t pos;
-	// char * buffer;
-	// size_t result;
-	// //fseek (arq, 0 , SEEK_END);
-	// //lSize = ftell (arq);
-	// //rewind (arq);
-	// //cout << sizeof(char)*lSize << endl;
-	// // buffer tem o tamanho de todo o arquivo em memória
-	// //buffer = (char*) malloc (sizeof(char)*lSize);
-	// //if (buffer == NULL) {fputs ("Memory error",stderr); exit (2);}
-	// //cout << sizeof(buffer) << endl;
-	// fgetpos(arq,&pos);
-	// buffer  = (char*)malloc(sizeof(char)*256);
-	
-	// // lembrando que result é o número de 
-	// // carabteres lidos bem sucedidos pelo fread
-	// fread(buffer,1,256,arq);
-	// cout << buffer << endl;
+	 FILE * arq;
+	 arq = fopen("/home/jonas/Documentos/SGBD/Tables/orders.dat","rb+");
+	 //long lSize;
+	 if(arq==NULL)
+		 exit(1);
+	 
+	 fpos_t pos;
+	 char * buffer;
+	 size_t result;
+	 //fseek (arq, 0 , SEEK_END);
+	 //lSize = ftell (arq);
+	 //rewind (arq);
+	 //cout << sizeof(char)*lSize << endl;
+	 // buffer tem o tamanho de todo o arquivo em memória
+	 //buffer = (char*) malloc (sizeof(char)*lSize);
+	 //if (buffer == NULL) {fputs ("Memory error",stderr); exit (2);}
+	 //cout << sizeof(buffer) << endl;
+	 fgetpos(arq,&pos);
+	 buffer  = (char*)malloc(sizeof(char)*256);
 
-	// char * clean;
-	// unsigned j=0;
-	// for(unsigned i=150;i<256;i++)
-	// {
-	// 	if(buffer[i] == '\n'){
-	// 		j=i;
-	// 		i=256;
-	// 	}
-	// }
-	// cout << j << endl;
-	// clean  = (char*)malloc(sizeof(char)*j);
-	// fsetpos(arq,&pos);
-	// fread(clean,1,j,arq);
-	// cout << clean << endl;
-	// free(clean);
-	// free(buffer);
+ 	// lembrando que result é o número de 
+ 	// carabteres lidos bem sucedidos pelo fread
+ 	fread(buffer,1,256,arq);
+ 	cout << buffer << endl;
+
+ 	char * clean;
+ 	unsigned j=0;
+ 	for(unsigned i=50;i<250;i++)
+ 	{
+ 		if(buffer[i] == '\n'){
+ 			j=i;
+ 			i=250;
+ 		}
+ 	}
+ 	cout << j << endl;
+	clean  = (char*)malloc(sizeof(char)*j);
+	fsetpos(arq,&pos);
+	fread(clean,1,j,arq);
+	cout << clean << endl;
+	free(clean);
+	free(buffer);
 	
-	HashJoin hash(argc,argv);
-	string match = "l_suppkey";
-	char * a = (char*)"l_orderkey,l_partkey,l_suppkey,l_linenumber,l_quantity,l_extendedprice,l_discount,l_tax,l_returnflag,l_linestatus,l_shipDATE,l_commitDATE,l_receiptDATE,l_shipinstruct,l_shipmode,l_comment";
-	hash.getKeyColumn(a,match);
+	int stop_s=clock();
+	cout << "time: " << (stop_s-start_s)/double(CLOCKS_PER_SEC) << endl;
+
+
+	//HashJoin hash(argc,argv);
+	//string match = "l_suppkey";
+	//char * a = (char*)"l_orderkey,l_partkey,l_suppkey,l_linenumber,l_quantity,l_extendedprice,l_discount,l_tax,l_returnflag,l_linestatus,l_shipDATE,l_commitDATE,l_receiptDATE,l_shipinstruct,l_shipmode,l_comment";
+	//hash.getKeyColumn(a,match);
 	//cout<<hash.getBinaryStringNumber(423)<<endl;
 	
+	//
+
 	//cout << hash.getBucketIndex("0100") << endl;
 
 	return EXIT_SUCCESS;
